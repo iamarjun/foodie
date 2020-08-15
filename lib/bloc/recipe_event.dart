8bd7db15ef192f2c10623c559be6f1d04f878f68
iamarjun <arjunmanoj1995@gmail.com>
2020-08-16 02:04:53 +0530

    describe the possible events

--- lib/bloc/recipe_event.dart
+++ lib/bloc/recipe_event.dart
@@ -2,7 +2,22 @@ part of 'recipe_bloc.dart';
 
 abstract class RecipeEvent extends Equatable {
   const RecipeEvent();
+}
+
+class GetRecipeList extends RecipeEvent {
+  final String recipeName;
+
+  const GetRecipeList(this.recipeName);
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [recipeName];
+}
+
+class GetRecipeDetail extends RecipeEvent {
+  final String recipeId;
+
+  const GetRecipeDetail(this.recipeId);
+
+  @override
+  List<Object> get props => [recipeId];
 }
