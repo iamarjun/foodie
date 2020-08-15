5c3eacc6c2918578c33af1fb56f498203f73aac3
iamarjun <arjunmanoj1995@gmail.com>
2020-08-16 02:12:17 +0530

    descripe the possible states

--- lib/bloc/recipe_bloc.dart
+++ lib/bloc/recipe_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:foodie/models/recipe.dart';
 
 part 'recipe_event.dart';
 part 'recipe_state.dart';
--- lib/bloc/recipe_state.dart
+++ lib/bloc/recipe_state.dart
@@ -2,9 +2,34 @@ part of 'recipe_bloc.dart';
 
 abstract class RecipeState extends Equatable {
   const RecipeState();
-  
+}
+
+class RecipeInitial extends RecipeState {
+  const RecipeInitial();
+
   @override
   List<Object> get props => [];
 }
 
-class RecipeInitial extends RecipeState {}
+class RecipeLoading extends RecipeState {
+  const RecipeLoading();
+
+  @override
+  List<Object> get props => [];
+}
+
+class RecipeLoaded extends RecipeState {
+  final Recipe recipe;
+  const RecipeLoaded(this.recipe);
+
+  @override
+  List<Object> get props => [recipe];
+}
+
+class RecipeError extends RecipeState {
+  final String message;
+  const RecipeError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
