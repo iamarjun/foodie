502d31efdbcc0867bff31468c58c449a6b6f1604
iamarjun <arjunmanoj1995@gmail.com>
2020-08-16 17:32:53 +0530

    setup api service and related classes

---
+++ lib/service/api_service.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:foodie/models/recipe.dart';
+import 'package:foodie/models/recipes.dart';
+import 'package:foodie/service/api_exceptions.dart';
+import 'package:http/http.dart';
+
+class ApiService {
+  static final ApiService _instance = ApiService._in();
+
+  factory ApiService() => _instance;
+
+  ApiService._in();
+
+  Future<List<Recipe>> fetchRecipes(String recipe) async {
+    var recipes;
+
+    try {
+      final response =
+          await get('https://recipesapi.herokuapp.com/api/search?q=$recipe');
+      recipes = Recipes.fromJson(_returnResponse(response)).recipes;
+    } on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+
+    return recipes;
+  }
+
+  dynamic _returnResponse(Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        print(responseJson);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+    }
+  }
+}
