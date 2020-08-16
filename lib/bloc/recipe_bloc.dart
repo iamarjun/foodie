b1ff30401ebd8863e0860cada1a433db54fc7029
iamarjun <arjunmanoj1995@gmail.com>
2020-08-16 16:47:36 +0530

    wire up the code in the bloc with the states and the events

--- lib/bloc/recipe_bloc.dart
+++ lib/bloc/recipe_bloc.dart
@@ -3,17 +3,28 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:foodie/models/recipe.dart';
+import 'package:foodie/repository/repository.dart';
 
 part 'recipe_event.dart';
 part 'recipe_state.dart';
 
 class RecipeBloc extends Bloc<RecipeEvent, RecipeState> {
-  RecipeBloc() : super(RecipeInitial());
+  final Repository recipeRepository;
+
+  RecipeBloc(this.recipeRepository) : super(RecipeInitial());
 
   @override
   Stream<RecipeState> mapEventToState(
     RecipeEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield RecipeLoading();
+    if (event is GetRecipeList) {
+      try {
+        final response = await recipeRepository.fetchRecipes(event.recipeName);
+        yield RecipeLoaded(response);
+      } catch (e) {
+        yield RecipeError(e.message);
+      }
+    }
   }
 }
--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -5,3 +5,19 @@ abstract class Repository {
   Future<Recipes> fetchRecipes(String recipe);
   Future<Recipe> fetchRecipeDetail(String recipeId);
 }
+
+
+class RecipeRepository extends Repository {
+  @override
+  Future<Recipe> fetchRecipeDetail(String recipeId) {
+      // TODO: implement fetchRecipeDetail
+      throw UnimplementedError();
+    }
+  
+    @override
+    Future<Recipes> fetchRecipes(String recipe) {
+    // TODO: implement fetchRecipes
+    throw UnimplementedError();
+  }
+
+}
