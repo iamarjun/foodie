b324d5a1595549ab24c648b0fbf75c24f9e12464
iamarjun <arjunmanoj1995@gmail.com>
2020-08-17 19:45:36 +0530

    design recipe detail widget in a bottom sheet style

---
+++ lib/ui/recipe_detail.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class RecipeDetail extends StatelessWidget {
+  const RecipeDetail({Key key}) : super(key: key);
+
+  final double _radius = 150.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Stack(
+            alignment: Alignment.topCenter,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                  top: _radius / 2.0,
+                ),
+
+                ///here we create space for the circle avatar to get out of the box
+                child: Container(
+                  height: 300.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8.0,
+                        offset: Offset(0.0, 2.0),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: _radius / 2,
+                          ),
+                          Text(
+                            '',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                          Text(
+                            '',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 28.0),
+                            child: Container(),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+
+              ///Image Avatar
+              Container(
+                width: _radius,
+                height: _radius,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 8.0,
+                      offset: Offset(0.0, 5.0),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Center(
+                    child: Container(
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(''),
+                        radius: _radius,
+                        backgroundColor: Colors.green[800],
+                      ),
+
+                      /// replace your image with the Icon
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
